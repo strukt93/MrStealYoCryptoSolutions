e728fe090f36eb5584aac44124306adb1efef0fc
strukt93 <strukt93@gmail.com>
2023-01-24 14:52:18 +0200

    Added solutions so far

---
+++ README.md
@@ -0,0 +1,11 @@
+# Mr Steal Yo Crypto CTF
+
+**A set of challenges to learn offensive security of smart contracts.** Featuring interesting challenges loosely (or directly) inspired by real world exploits.
+
+Created by [@0xToshii](https://twitter.com/0xToshii)
+
+## Play
+
+Visit [mrstealyocrypto.xyz](https://mrstealyocrypto.xyz)
+
+Note: main branch includes solutions, run <code>git checkout implement</code> to see problems without their respective solutions
---
+++ hardhat.config.ts
@@ -0,0 +1,25 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true 
+    }
+  },
+  solidity: {
+    compilers: [
+      { version: "0.8.7" },
+      { version: "0.8.6" },
+      { version: "0.6.6" },
+      { version: "0.5.17" },
+      { version: "0.5.16" },
+      { version: "0.4.11" }
+    ],
+  },
+  mocha: {
+    timeout: 300 * 1e3,
+  }
+};
+
+export default config;
